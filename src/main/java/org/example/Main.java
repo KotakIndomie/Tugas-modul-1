e9c39770cad0e5bd8a94512cba1ee81fc7a6fc00
KotakIndomie <poryu515@gmail.com~>
2024-02-27 12:48:56 +0800

    Tugas 1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,67 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Library System");
+            System.out.println("1. Login sebagai admin");
+            System.out.println("2. Login sebagai mahasiswa");
+            System.out.println("3. Keluar");
+            System.out.println("Masukkan pilihan Anda: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    loginAdmin(scanner);
+                    break;
+                case 2:
+                    loginMahasiswa(scanner);
+                    break;
+                case 3:
+                    System.out.println("Keluar dari program...");
+                    break;
+                default:
+                    System.out.println("Pilihan salah");
+            }
+
+            if (choice == 3) {
+                break; // Keluar dari loop
+            }
+        }
+
+        scanner.close();
+    }
+
+    private static void loginAdmin(Scanner scanner) {
+        System.out.println("Masukkan username: ");
+        String username = scanner.nextLine();
+
+        System.out.println("Masukkan password: ");
+        String password = scanner.nextLine();
+
+        if (username.equals("admin") && password.equals("admin")) {
+            System.out.println("Login admin berhasil!");
+        } else {
+            System.out.println("Username atau password salah!");
+        }
+    }
+
+
+    private static void loginMahasiswa(Scanner scanner) {
+        System.out.println("Masukkan NIM: ");
+        String nim = scanner.nextLine();
+
+        if (nim.length() == 15) {
+            System.out.println("Login mahasiswa berhasil!");
+        } else {
+            System.out.println("Panjang NIM harus 15 karakter!");
+        }
+    }
+}
